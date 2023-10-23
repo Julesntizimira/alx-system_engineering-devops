83747b7e6722f19a0c2273e8dbbd887d7ad28186
Julesntizimira <ntizimijules5@gmail.com>
2023-10-23 11:40:56 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''a Python script that, using this REST API,
+   for a given employee ID, returns information
+   about his/her TODO list progress
+'''
+import requests
+import json
+from sys import argv
+
+
+r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                 .format(argv[1]))
+res = r.json()
+
+res_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos = res_todos.json()
+
+titles = []
+
+for todo in todos:
+    if todo.get('userId') == eval(argv[1]):
+        titles.append(todo['title'])
+
+print('Employee {} is done with tasks({}/{}):'
+      .format(res.get('name'), len(titles), len(todos)))
+for title in titles:
+    print('\t {}'.format(title))
