7a27e02a9dd6cfb880c9a6f176fbe828f834bb39
Julesntizimira <ntizimijules5@gmail.com>
2023-10-23 11:58:51 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,14 +13,14 @@ r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                  .format(argv[1]))
 res = r.json()
 
-res_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+res_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(argv[1]))
 todos = res_todos.json()
 
 titles = []
-
 for todo in todos:
-    if todo.get('userId') == eval(argv[1]):
-        titles.append(todo['title'])
+    if todo.get('completed'):
+        titles.append(todo.get('title'))
 
 print('Employee {} is done with tasks({}/{}):'
       .format(res.get('name'), len(titles), len(todos)))
