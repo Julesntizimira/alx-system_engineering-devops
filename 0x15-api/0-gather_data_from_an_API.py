6d3d1f21b48f8ed3e9df2407080f906c9da8c7ce
Julesntizimira <ntizimijules5@gmail.com>
2023-10-23 12:12:18 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,26 +3,23 @@
    for a given employee ID, returns information
    about his/her TODO list progress
 '''
-import json
-import requests
-import sys
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+    argv = sys.argv
+    td = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+    us = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    r = requests.get(us)
+    res = r.json()
 
-
-argv = sys.argv
-r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                 .format(argv[1]))
-res = r.json()
-
-res_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                         .format(argv[1]))
-todos = res_todos.json()
-
-titles = []
-for todo in todos:
-    if todo.get('completed'):
-        titles.append(todo.get('title'))
-
-print('Employee {} is done with tasks({}/{}):'
-      .format(res.get('name'), len(titles), len(todos)))
-for title in titles:
-    print('\t {}'.format(title))
+    res_todos = requests.get(td)
+    todos = res_todos.json()
+    titles = []
+    for todo in todos:
+        if todo.get('completed'):
+            titles.append(todo.get('title'))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(res.get('name'), len(titles), len(todos)))
+    for title in titles:
+        print('\t {}'.format(title))
