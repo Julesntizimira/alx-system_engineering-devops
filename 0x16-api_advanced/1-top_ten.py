b5fe4ec2fb1ea03733a2c1d52bbe3148fe02bc12
Julesntizimira <ntizimijules5@gmail.com>
2023-11-07 11:12:53 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,13 @@ def top_ten(subreddit):
         listed for a given subreddit."""
     if subreddit is None or not isinstance(subreddit, str):
         print(None)
+        return
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
     response = get(url, headers=user_agent)
+    if response.status_code != '200':
+        print(None)
+        return
     results = response.json()
     try:
         res = results.get('data').get('children')
