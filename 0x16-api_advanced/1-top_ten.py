35c285ea7b7b67281dbe648ba4137f34bf0abc24
Julesntizimira <ntizimijules5@gmail.com>
2023-11-07 11:24:02 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,22 +7,20 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts
         listed for a given subreddit."""
     if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
+        print("None")
+
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    if response.status_code == '404':
-        print(None)
-        return
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
     results = response.json()
+
     try:
-        res = results.get('data').get('children')
-        i = 0
-        for ps in res:
-            if i >= 10:
-                break
-            print(ps.get('data').get('title'))
-            i += 1
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
     except Exception:
-        return print(None)
+        print("None")
