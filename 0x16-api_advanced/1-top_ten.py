0422f603aeecb768d5326c26f5633b9f93fa5d4f
Julesntizimira <ntizimijules5@gmail.com>
2023-11-07 11:04:38 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines top_ten()"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts
+        listed for a given subreddit."""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+    try:
+        res = results.get('data').get('children')
+        i = 0
+        for ps in res:
+            if i >= 10:
+                break
+            print(ps.get('data').get('title'))
+            i += 1
+    except Exception:
+        return 0
